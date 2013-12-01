e41c906ff6c8663ea558beccc90ea0e85ca75a50
Michael Currie <mcurrie@bruceforceresearch.com>
2013-11-30 22:30:21 -0700

    Rewrote get_features_rewritten as wormpy_example
    I more clearly wrote it as code that's merely intended to illustrate the
    use of the wormpy module.
    I added all functionality from example_worm into wormpy_example.
    I like the name wormpy_example instead of get_features_rewritten which
    is a bit cryptic.

---
+++ wormpy_example.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 26 22:48:17 2013
+
+@author: Michael Currie
+A translation of Matlab code written by Jim Hokanson,
+in the SegwormMatlabClasses GitHub repo.  Original code path:
++seg_worm / @feature_calculator / get_features_rewritten.m
+"""
+
+import os
+import wormpy
+
+def main():
+  """
+    This is an example illustrating use of the classes in the wormpy module.
+    We load the skeleton and other basic data from a worm HDF5 file,
+    optionally animate it using matplotlib, and also    
+    re-create the features information by deriving them from the basic data.
+  """
+  # DEFAULT SETTING
+  NORM_PATH = None  
+  # JIM'S TESTING CODE
+  # on PC:
+  #NORM_PATH = 'F:\worm_data\segworm_data\video\testing_with_GUI\.data\mec-4 (u253) off food x_2010_04_21__17_19_20__1_seg\normalized';
+  # on MAC:
+  #NORM_PATH = '/Users/jameshokanson/Dropbox/worm_data/video/testing_with_GUI/.data/mec-4 (u253) off food x_2010_04_21__17_19_20__1_seg/normalized'
+  
+  normalized_worm = create_example_normalized_worm(NORM_PATH)
+  
+  # AT THIS POINT WE COULD ANIMATE THE WORM IF WE WANTED:
+  #normalized_worm.interpolate_dropped_frames()  
+  #normalized_worm.animate()  
+  #normalized_worm.save_to_mp4("worm_animation.mp4")
+  
+  worm_features = get_features(normalized_worm)
+  
+  # TODO: do something more useful with the output here than just print...
+  print(worm_features)
+
+
+
+def get_features(normalized_worm):
+  """ 
+    INPUT: normalized_worm, an instance of wormpy.NormalizedWorm
+    OUTPUT: an instance of wormpy.WormFeatures
+    From the basic information in normalized_worm,
+    create an instance of WormFeatures, which contains all our features data.
+  """
+  worm_features = wormpy.WormFeatures(normalized_worm)
+  
+  return worm_features
+
+
+
+def create_example_normalized_worm(norm_folder = None):
+  """
+    This function takes a path, and loads a worm HDF5 experiment file,
+    only looking at the skeleton and other basic information.
+  """
+
+  # DEBUG: change to Jim's class later
+  normalized_worm = wormpy.NormalizedWorm()
+
+  # If no folder was specified for the worm, use the 
+  # current working directory
+  if(norm_folder == None):
+    norm_folder = os.path.abspath(os.getcwd())
+  
+  # DEBUG: hardcoded for now.
+  worm_file_path = os.path.join(norm_folder, 
+                               "unc-8 (rev) on food " +
+                               "R_2010_03_19__09_14_57___2___2_features.mat")
+
+  eigen_worm_file_path = os.path.join(norm_folder, 
+                               "masterEigenWorms_N2.mat")
+
+  normalized_worm.load_worm(worm_file_path)
+  normalized_worm.load_eigen_worms(eigen_worm_file_path)
+  
+  return normalized_worm
+  
+
+if(__name__ == '__main__'):
+  main()
