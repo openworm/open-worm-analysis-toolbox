9f5c65dc2790e1c0f7c2bfcc4aa01d02ac9ebcf5
Michael Currie <mcurrie@bruceforceresearch.com>
2013-12-17 19:06:31 -0700

    Added config file and feature_helpers.py

--- wormpy/WormFeatures.py
+++ wormpy/WormFeatures.py
@@ -4,14 +4,16 @@ Created on Wed Nov 27 22:36:32 2013
 
 @author: Michael
 
-NOTE: REQUIRES numpy.version.full_version >= '1.8' 
-since numpy.nanmean is only available after that version.
-(http://docs.scipy.org/doc/numpy-dev/reference/generated/numpy.nanmean.html)
-
 A translation of Matlab code written by Jim Hokanson,
 in the SegwormMatlabClasses GitHub repo.  Original code path:
 SegwormMatlabClasses / 
 +seg_worm / @feature_calculator / get_features_rewritten.m
+
+NOTE: REQUIRES numpy.version.full_version >= '1.8' 
+since numpy.nanmean is only available after that version.
+(http://docs.scipy.org/doc/numpy-dev/reference/generated/numpy.nanmean.html)
+Alternatively, you can just install nanfunctions.py 
+(see instructions in ..//README.md in this repo)
 
 *** For +seg_worm / @feature_calculator / getPostureFeatures.m,
 *** here are some renamed variables:
@@ -25,6 +27,8 @@ n_fields = length(FIELDS) = len(self.normal_partitions().keys())
 
 """
 import numpy as np
+from wormpy.config import *
+from wormpy.feature_helpers import *
 
 # TODO: WormFeatures should INHERIT from NormalizedWorm
 
@@ -34,35 +38,33 @@ class WormFeatures:
   """
   normalized_worm = None
   
+  features = None
+  
   morphology = None  # a python dictionary
   locomotion = None
   posture = None
   path = None
-  FPS = None  
-  N_ECCENTRICITY = None
+
   
   def __init__(self, nw):
-    self.FPS = 20               # TODO: get this from higher up..
-    self.N_ECCENTRICITY = 50  # TODO: get this from higher up..
-
     self.normalized_worm = nw
+    
     self.get_morphology_features()
     self.get_posture_features()    
     self.get_locomotion_features()
+    self.get_path_features()
     
-    
-
-
+      
   def get_morphology_features(self):
     """
     Translation of: SegwormMatlabClasses / 
     +seg_worm / @feature_calculator / getMorphologyFeatures.m
-
+  
     Nature Methods Description
     =======================================================================
- 
+   
     Morphology Features 
- 
+   
     1. Length. Worm length is computed from the segmented skeleton by
     converting the chain-code pixel length to microns.
     
@@ -73,9 +75,9 @@ class WormFeatures:
     
     3. Area. The worm area is computed from the number of pixels within the
     segmented contour. The sum of the pixels is converted to microns2.
- 
+   
     4. Area/Length.
- 
+   
     5. Midbody Width/Length.
     
     get_morphology_features:
@@ -104,7 +106,7 @@ class WormFeatures:
                                        self.morphology['length']
     self.morphology['widthPerLength'] = self.morphology['width']['midbody'] / \
                                         self.morphology['length']
-  
+    
   def num_fields(self):
     return 
   
@@ -134,50 +136,32 @@ class WormFeatures:
     self.posture = {}  
 
     # *** 1. Bends ***
-    # We care only about the head, neck, midbody, hips and tail 
-    # (i.e. the 'normal' way to partition the worm)
-    p = self.normalized_worm.get_partition_subset('normal')
-    
-    bends = {}
-    
-    for partition_key in p.keys():
-      # retrieve the part of the worm we are currently looking at:
-      bend_angles = self.normalized_worm.get_partition(partition_key, 'angles')
-      
-      bend_metrics_dict = {}
-      # shape = (n):
-      bend_metrics_dict['mean'] = np.nanmean(a=bend_angles, axis = 0) 
-      bend_metrics_dict['std_dev'] = np.nanstd(a=bend_angles, axis = 0)
-      
-      # Sign the standard deviation (to provide the bend's 
-      # dorsal/ventral orientation):
-      
-      # First find all entries where the mean is negative
-      mask = np.ma.masked_where(condition=bend_metrics_dict['mean'] < 0,
-                                a=bend_metrics_dict['mean']).mask
-      # Now create a numpy array of -1 where the mask is True and 1 otherwise
-      sign_array = -np.ones(np.shape(mask)) * mask + \
-                   np.ones(np.shape(mask)) * (~mask)
-      # Finally, multiply the std_dev array by our sign_array
-      bend_metrics_dict['std_dev'] = bend_metrics_dict['std_dev'] * sign_array
-      
-      bends[partition_key] = bend_metrics_dict
-      
-    # The final bends dictionary now contains a mean and standard 
-    # deviation for the head, neck, midbody, hips and tail.
+
     # Now that we've populated the bends dictionary, add it to the posture
     # dictionary.
-    self.posture['bends'] = bends
+    self.posture['bends'] = get_bends(self.normalized_worm)
     
     # *** 2. Eccentricity & Orientation ***
-    
+    eccentricity_and_orientation = \
+            get_eccentricity(self.normalized_worm.contour_x(), 
+                             self.normalized_worm.contour_y())
+    self.posture['eccentricity'] = eccentricity_and_orientation['eccentricity']
+    self.posture['orientation'] = eccentricity_and_orientation['orientation']
     
     # *** 3. Amplitude, Wavelengths, TrackLength, Amplitude Ratio ***
+    amp_wave_track = get_amplitude_and_wavelength(self.posture['orientation'],
+                                                  self.normalized_worm.skeletons_x(),
+                                                  self.normalized_worm.skeletons_y())
+    self.posture['amplitude'] = amp_wave_track['amplitude']
+    self.posture['wavelength'] = amp_wave_track['wavelength']
+    self.posture['track_length'] = amp_wave_track['track_length']
 
+    # TODO: change this to return multiple values as in 
+    # http://stackoverflow.com/questions/354883/how-do-you-return-multiple-values-in-python
 
     # *** 4. Kinks ***
 
-    # *** 5. Coils (NOT YET FINISHED BY JIM) ***
+    # *** 5. Coils ***
 
     # *** 6. Directions ***
 
@@ -206,3 +190,7 @@ class WormFeatures:
 
     """    
     pass
+
+
+
+
---
+++ wormpy/config.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 17 16:20:47 2013
+
+@author: @MichaelCurrie
+
+The original Schafer Lab code contained many hardcoded values.  These are
+tracked in a central location here.
+Some hardcoded values were not implemented in this open-source translation.
+these are shown here
+"""
+
+__ALL__ = ['FPS', 'N_ECCENTRICITY']
+
+
+""" FEATURE CONFIGS """
+
+# Frames Per Second
+FPS = 20                
+
+# Grid size for estimating eccentricity, this is the
+# max # of points that will fill the wide dimension.
+# (scalar) The # of points to place in the long dimension. More points
+# gives a more accurate estimate of the ellipse but increases
+# the calculation time.
+N_ECCENTRICITY = 50     
+
+POSTURE_AMPLITURE_AND_WAVELENGTH = { \
+  'N_POINTS_FFT': 512, 
+  # NOTE: Unfortunately the distance is in normalized
+  # frequency units (indices really), not in real frequency units
+  'MIN_DIST_PEAKS': 5, 
+  'WAVELENGTH_PCT_MAX_CUTOFF': 0.5,  # TODO: describe
+  'WAVELENGTH_PCT_CUTOFF': 2}        # TODO: describe
+  
+POSTURE_AMPLITURE_AND_WAVELENGTH['HALF_N_FFT'] = \
+  POSTURE_AMPLITURE_AND_WAVELENGTH['N_POINTS_FFT']/2
+
