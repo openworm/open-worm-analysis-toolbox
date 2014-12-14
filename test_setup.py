f40baa963fa86142b8c96fa5da61c6755d1abbec
Jim Hokanson <jim.hokanson@gmail.com>
2014-12-14 14:04:45 -0500

    Added file test_setup.py - it is not finished

---
+++ test_setup.py
@@ -0,0 +1,7 @@
+#TODO: This file should look for all package dependencies and setup issues
+#and provide detailed information as to how to handle things when there are problems
+
+
+http://www.lfd.uci.edu/~gohlke/pythonlibs/#matplotlib
+#http://www.lfd.uci.edu/~gohlke/pythonlibs/#scipy
+#http://www.lfd.uci.edu/~gohlke/pythonlibs/#shapely
