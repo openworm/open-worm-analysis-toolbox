dc535bda1f088c1be252c0032901bb1caaeb5d3d
Michael Currie <mcurrie@bruceforceresearch.com>
2015-05-17 12:27:18 -0600

    Fixing normalized worm unit test

--- tests/unit_test1.py
+++ tests/unit_test1.py
@@ -10,7 +10,7 @@ import sys
 # import of movement_validation while running this as 
 # a top-level script (i.e. with __name__ = '__main__')
 sys.path.append('..') 
-#from movement_validation import NormalizedWorm
+from movement_validation import NormalizedWorm
 
 
 def test_simply():
