3d93497e7a1aebd722f114df2de5fde21a0eae21
Jim Hokanson <jim.hokanson@gmail.com>
2015-06-14 23:03:26 -0400

    Update morphology_features.py
    Accidentally used the wrong morphology_features version, using the correct one now

--- movement_validation/features/morphology_features.py
+++ movement_validation/features/morphology_features.py
@@ -1,58 +1,53 @@
 # -*- coding: utf-8 -*-
 """
 morphology_features.py
-
-Any more complex morphology features requiring classes are defined here.
-
 """
 
 import numpy as np
 
 from .. import utils
+
 from . import feature_comparisons as fc
 
 class Widths(object):
     """
-    Stores the mean width of various partitions of the worm, for each frame.
-    
     Attributes
     ----------    
-    head:    The mean width of the worm along the 'head' partition
-    midbody: The mean width of the worm along the 'midbody' partition
-    tail:    The mean width of the worm along the 'tail' partition
-
+    head :
+    midbody :
+    tail :
     """
     
-    def __init__(self, nw=None):
+    fields = ('head', 'midbody', 'tail')
+    
+    def __init__(self,features_ref):
         """
         Parameters
         ----------
-        nw: An instance of NormalizedWorm
-
+        features_ref : WormFeatures
+        
+        Note the current approach just computes the mean of the different 
+        body section widths. Eventually this should be computed in this class.
         """
-        self.fields = ('head', 'midbody', 'tail')
-
-        if nw is not None:
-            for partition in self.fields:
-                setattr(self, partition, 
-                        np.mean(nw.get_partition(partition, 'widths'), 0))
+        nw = features_ref.nw
+    
+        for partition in self.fields:
+            setattr(self,partition, np.mean(nw.get_partition(partition, 'widths'),0))
     
     @classmethod
-    def from_disk(cls, width_ref):
-        widths_instance = cls()
+    def from_disk(cls,width_ref):
 
-        for partition in widths_instance.fields:
-            setattr(widths_instance, partition, 
-                    utils._extract_time_from_disk(width_ref, partition))
+        self = cls.__new__(cls)
+
+        for partition in self.fields:
+            setattr(self,partition, utils._extract_time_from_disk(width_ref,partition))
     
-        return widths_instance
+        return self
         
     def __eq__(self, other):
-        return \
-            fc.corr_value_high(self.head, other.head, 'morph.width.head') and \
-            fc.corr_value_high(self.midbody, other.midbody, 
-                               'morph.width.midbody') and \
+        return fc.corr_value_high(self.head, other.head, 'morph.width.head') and \
+            fc.corr_value_high(self.midbody, other.midbody, 'morph.width.midbody') and \
             fc.corr_value_high(self.tail, other.tail, 'morph.width.tail')
 
     def __repr__(self):
-        return utils.print_object(self)    
+        return utils.print_object(self)  
