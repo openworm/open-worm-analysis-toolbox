cda9f6c6e55f8eb5bc1be1b16d63838158933577
Jim Hokanson <jim.hokanson@gmail.com>
2015-06-16 21:28:18 -0400

    Update unit_test2.py
    [ci skip]

--- tests/unit_test2.py
+++ tests/unit_test2.py
@@ -78,4 +78,4 @@ if __name__ == '__main__':
     #start_time = utils.timing_function()
     test_main()
     #print("Time elapsed: %.2f seconds" % 
-          (utils.timing_function() - start_time))
+    #(utils.timing_function() - start_time))
