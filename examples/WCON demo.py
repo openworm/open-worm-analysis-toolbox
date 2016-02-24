d44835a063d12ab68722aa92834e9b718bdff5e4
Michael Currie <mcurrie@bruceforceresearch.com>
2016-02-23 19:35:40 -0700

    Working on WCON serialization

--- examples/WCON demo.py
+++ examples/WCON demo.py
@@ -14,7 +14,7 @@ sys.path.append('..')
 import open_worm_analysis_toolbox as mv
 
 import wcon
-
+import pandas as pd
 
 class BasicWorm2(wcon.WCONWorms):
     pass
@@ -29,6 +29,17 @@ if __name__ == '__main__':
                      "example_contour_and_skeleton_info.mat")
     bw = mv.BasicWorm.from_schafer_file_factory(
         schafer_bw_file_path)
+
+    with open('testfile.wcon', 'w') as f:
+        f.write('{\n    {"units":"t":"s","x":"mm","y":"mm"},\n    {"data":[{')
+        num_timeframes = len(bw.h_dorsal_contour)
+        f.write('"t":%s,' % str(list(range(num_timeframes))))
+        f.write('\n"x":[')
+        for frame_index in range(10): #num_timeframes):
+            f.write('%s,' % repr(bw.h_dorsal_contour))
+        f.write(']}]}}')
+        
+    #for bw.h_skeleton
 
     #nw = mv.NormalizedWorm.from_BasicWorm_factory(bw)
 
