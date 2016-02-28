8b83f371b35ddc5b739531efbad3040bff50472a
Michael Currie <mcurrie@bruceforceresearch.com>
2016-02-28 02:05:28 -0700

    Working on WCON example

--- examples/WCON demo.py
+++ examples/WCON demo.py
@@ -15,12 +15,17 @@ import open_worm_analysis_toolbox as mv
 
 import wcon
 import pandas as pd
+import numpy as np
+
+import pickle
 
 class BasicWorm2(wcon.WCONWorms):
     pass
 
 if __name__ == '__main__':
     warnings.filterwarnings('error')
+    print('RUNNING TEST ' + os.path.split(__file__)[1] + ':')
+    start_time = mv.utils.timing_function()
 
     base_path = os.path.abspath(
         mv.user_config.EXAMPLE_DATA_PATH)
@@ -30,15 +35,57 @@ if __name__ == '__main__':
     bw = mv.BasicWorm.from_schafer_file_factory(
         schafer_bw_file_path)
 
+    #with open('testfile.pickle', 'wb') as f:
+    #    pickle.dump(bw, f)
+
+    w = wcon.WCONWorms()
+    w.units = {"t":"s","x":"mm","y":"mm"}
+    for key in w.units:
+        w.units[key] = wcon.MeasurementUnit.create(w.units[key])
+    w.units['aspect_size'] = wcon.MeasurementUnit.create('')
+    
+    w.metadata = {"lab":{"location":"MRC Laboratory of Molecular Biology, "
+                                    "Hills Road, Cambridge, CB2 0QH, "
+                                    "United Kingdom","name":"Schafer Lab"}}
+
+    num_frames = len(bw.h_dorsal_contour)
+    worm_data_segment = {'id':0, 't':list(range(num_frames))}
+
+    h_dorsal_contour = np.array(bw.h_dorsal_contour)
+    empty_xy = np.empty((2,0))
+
+    # Replace None entries with empty articulations for both x and y dimensions
+    for frame_index in range(num_frames):
+        if h_dorsal_contour[frame_index] is None:
+            h_dorsal_contour[frame_index] = empty_xy
+
+    #none_fixer = np.vectorize(lambda a: empty_xy if (a is None) else a)
+    #h_dorsal_contour = none_fixer(h_dorsal_contour)
+    
+    for dimension_index, dimension in enumerate(['x', 'y']):
+        worm_data_segment[dimension] = [list(h_dorsal_contour[findex][0]) 
+                                        for findex in range(num_frames)]
+
+    w.data = wcon.wcon_data.parse_data(worm_data_segment)
+
+    w.save_to_file('testfile2.wcon', pretty_print=True)
+
+    print("Time elapsed: %.2f seconds" %
+          (mv.utils.timing_function() - start_time))   
+    
+    """
     with open('testfile.wcon', 'w') as f:
-        f.write('{\n    {"units":"t":"s","x":"mm","y":"mm"},\n    {"data":[{')
+        f.write('{\n    ,\n    {"data":[{')
         num_timeframes = len(bw.h_dorsal_contour)
         f.write('"t":%s,' % str(list(range(num_timeframes))))
-        f.write('\n"x":[')
-        for frame_index in range(10): #num_timeframes):
-            f.write('%s,' % repr(bw.h_dorsal_contour))
-        f.write(']}]}}')
-        
+        for dimension_index, dimension in enumerate(['x', 'y']):
+            f.write('\n"x":[')
+            for frame_index in range(1): #num_timeframes):
+                    f.write('%s,' % repr(list(bw.h_dorsal_contour[frame_index][dimension_index])))
+            f.write(']')
+        f.write('}]}}')
+    """
+
     #for bw.h_skeleton
 
     #nw = mv.NormalizedWorm.from_BasicWorm_factory(bw)
